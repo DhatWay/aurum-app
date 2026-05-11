b7c9b3239219609410836df6abcf54d3a9349bf0
iamthevgod <dhatway2024@gmail.com>
2026-05-10 23:34:32 -0400

    Update sw.js

--- sw.js
+++ sw.js
@@ -9,11 +9,11 @@ const ASSETS = [
   '/aurum-app/news.html',
   '/aurum-app/tutorial.html',
   '/aurum-app/help.html',
+  '/aurum-app/assistant.html',
   '/aurum-app/manifest.json',
   '/aurum-app/icon-192.png',
   '/aurum-app/icon-512.png'
 ];
-
 self.addEventListener('install', event => {
   event.waitUntil(
     caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
